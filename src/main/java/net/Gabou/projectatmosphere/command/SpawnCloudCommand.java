d1f90fa8baf2eb628af3861e14fbcb0717f4563b
xGabou <99047760+xGabou@users.noreply.github.com>
2025-05-02 13:25:31 -0400

    Refactor BiomeTempConfig to enhance temperature range documentation and populate biome ranges

--- src/main/java/net/Gabou/projectatmosphere/command/SpawnCloudCommand.java
+++ src/main/java/net/Gabou/projectatmosphere/command/SpawnCloudCommand.java
@@ -2,6 +2,7 @@ package net.Gabou.projectatmosphere.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import net.Gabou.projectatmosphere.util.SereneTempToCelcius;
+import net.Gabou.projectatmosphere.util.TemperatureUtils;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.BlockPos;
@@ -67,6 +68,28 @@ public class SpawnCloudCommand {
                             false
                     );
                     var tempCelcius = SereneTempToCelcius.SereneTempToCelcius(temp);
+                    context.getSource().sendSuccess(
+                            () -> Component.literal("Current temperature in celcius: " + tempCelcius),
+                            false
+                    );
+                    return 1;
+                }));
+        dispatcher.register(Commands.literal("gt")
+                .requires(source -> source.hasPermission(2))
+                .executes(context -> {
+                    ServerLevel level = context.getSource().getLevel();
+                    var seasonData = SeasonHandler.getSeasonSavedData(level);
+                    SeasonTime time = new SeasonTime(seasonData.seasonCycleTicks);
+                    int subSeasonDuration = ModConfig.seasons.subSeasonDuration;
+                    Object[] var10001 = new Object[5];
+                    String var10004 = time.getSubSeason().toString();
+                    var10001[0] = Component.translatable("desc.sereneseasons." + var10004.toLowerCase(Locale.ROOT));
+                    var10001[1] = time.getDay() % subSeasonDuration + 1;
+                    var10001[2] = subSeasonDuration;
+                    var10001[3] = time.getSeasonCycleTicks() % time.getDayDuration();
+                    var10001[4] = time.getDayDuration();
+                    BlockPos onPos = context.getSource().getPlayer().getOnPos();
+                    var tempCelcius = TemperatureUtils.getRealTemperature(level, onPos);
                     context.getSource().sendSuccess(
                             () -> Component.literal("Current temperature in celcius: " + tempCelcius),
                             false
