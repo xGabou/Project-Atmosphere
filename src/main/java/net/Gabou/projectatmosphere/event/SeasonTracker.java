09367bb0fe9aa5305c4517b206e78a6444b0566c
Gabriel Galipeau <gabgalip@gmail.com>
2025-09-25 19:29:21 -0400

    fix: update mod version to 0.5.4.1 and improve season change handling

--- src/main/java/net/Gabou/projectatmosphere/event/SeasonTracker.java
+++ src/main/java/net/Gabou/projectatmosphere/event/SeasonTracker.java
@@ -3,6 +3,7 @@ package net.Gabou.projectatmosphere.event;
 import com.Gabou.sereneseasonsplus.util.EnvironmentHelper;
 import glitchcore.event.EventManager;
 import net.minecraft.server.level.ServerLevel;
+import sereneseasons.api.season.Season;
 import sereneseasons.api.season.SeasonChangedEvent;
 import net.Gabou.projectatmosphere.manager.AtmosphereManager;
 
@@ -13,7 +14,11 @@ public class SeasonTracker {
             if (event.getLevel() instanceof ServerLevel serverLevel) {
                 if (event.getNewSeason().getSeason() != event.getPrevSeason().getSeason()) {
                     AtmosphereManager.onSeasonChange(serverLevel);
-                    EnvironmentHelper.onSeasonChange(serverLevel);
+                    Season.SubSeason oldSeason = event.getPrevSeason();
+                    Season.SubSeason newSeason = event.getNewSeason();
+                    if (newSeason != oldSeason) {
+                        EnvironmentHelper.onSeasonChange(serverLevel,Math.abs(newSeason.ordinal() - oldSeason.ordinal()) != 1);
+                    }
                 }
             }
         });
@@ -22,7 +27,12 @@ public class SeasonTracker {
             if (event.getLevel() instanceof ServerLevel serverLevel) {
                 if (event.getNewSeason() != event.getPrevSeason()) {
                     AtmosphereManager.onSeasonChange(serverLevel);
-                    EnvironmentHelper.onSeasonChange(serverLevel);
+                    Season.TropicalSeason oldSeason = event.getPrevSeason();
+                    Season.TropicalSeason newSeason = event.getNewSeason();
+                    boolean b = Math.abs(newSeason.ordinal() - oldSeason.ordinal()) != 1;
+                    if (newSeason != oldSeason) {
+                        EnvironmentHelper.onSeasonChange(serverLevel, b);
+                    }
                 }
             }
         });
